26a0aae6c8014700b8cffee0cd99138453327123
WorkPlace <dykim001128@naver.com>
2025-09-10 11:37:35 +0900

    chore: 약간 수정

--- media-server/mediaConfig.ts
+++ media-server/mediaConfig.ts
@@ -1,10 +1,12 @@
-const NodeMediaServer = require('node-media-server');
+const  NodeMediaServer = require ( "node-media-server" ); 
 
-const config = {
-  rtmp: { port: 1935 },
-  http: { port: 8000, allow_origin: '*' },
-  trans: { ffmpeg: '/usr/bin/ffmpeg', tasks: [{ app: 'live', hls: true }] }
-};
+// Node Media Server에는 구성 매개변수가 필요합니다. 
+const config = {}; 
 
-const nms = new NodeMediaServer(config);
-nms.run();
+const nms = new  NodeMediaServer (config); 
+
+// run 메서드를 사용하여 미디어 서버를 시작합니다.
+ nms.run();
+
+ // https://lemonpie313.tistory.com/239 참고
+ // 
