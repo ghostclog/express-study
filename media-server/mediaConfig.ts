356beeeef518a29f23c9ab482dc97817040aca65
WorkPlace <dykim001128@naver.com>
2025-09-10 14:35:30 +0900

    chore: 설정 공부 및 이것저것 타입 에러 잡는 작업 진행

--- media-server/mediaConfig.ts
+++ media-server/mediaConfig.ts
@@ -1,12 +1,40 @@
-const  NodeMediaServer = require ( "node-media-server" ); 
+import NodeMediaServer from "node-media-server"; 
 
-// Node Media Server에는 구성 매개변수가 필요합니다. 
-const config = {}; 
+const config = {
+    rtmp:{
+        port: 1935,
+        chunk_size: 60000,
+        gop_cache: true,
+        ping: 30,
+        ping_timeout: 60
+    },
+    http:{
+        port: 8000,
+        allow_origin: "*",
+        mediaroot: "./media"
+    },
+    trans: {
+        ffmpeg: '/usr/bin/ffmpeg',
+        tasks: [
+          {
+            app: 'live',
+            hls: true,
+            dash: true
+          }
+        ]
+    }
+    
+}; 
 
-const nms = new  NodeMediaServer (config); 
 
-// run 메서드를 사용하여 미디어 서버를 시작합니다.
- nms.run();
 
- // https://lemonpie313.tistory.com/239 참고
- // 
+// 'mediaroot' 속성이 http 설정에 필요하므로 추가합니다.
+
+const nms = new NodeMediaServer(config);
+
+nms.run();
+
+// https://lemonpie313.tistory.com/239 참고
+ // https://hippalus.tistory.com/658 참고
+
+ // 참고 : https://github.com/illuspas/Node-Media-Server
